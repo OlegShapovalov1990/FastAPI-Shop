7474fed5f3b276b1009b43aff34edb30c2456562
oleg9 <olega_djan@mail.ru>
2025-02-24 19:48:40 +0300

    create database config

--- ShopApp-fastapi/core/config.py
+++ ShopApp-fastapi/core/config.py
@@ -1,11 +1,21 @@
 from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
+
 
 
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False,
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
 class ApiPrefix(BaseModel):
     prefix: str = "/api"
@@ -16,6 +26,7 @@ class ApiPrefix(BaseModel):
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 
